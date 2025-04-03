d40943db6a8d76f72e7e2486039fe33d66f14d6a
aangelamz <amunozsafagranada@gmail.com>
2025-04-03 20:38:42 +0200

    cambios

--- src/matematicas.h
+++ src/matematicas.h
@@ -8,7 +8,14 @@ const double PI_ENTRE_CIENTO_80= PI / 180;
 const double CIENTO_80_ENTRE_PI = 180 / PI;
 
 
-// Mi funcion que pasa de cadena a numero
+/*! @brief Conversión de una cadena a número
+
+Esta función convierte una cadena de tipo cadena-C a números. Si es posible la conversión, al ser un bool
+nos devuelve true, si no, false, y el resultado lo devolverá en "n".
+Admite un signo positivo o negativo delante y un punto decimal. También admite espacios en blanco al comienzo 
+o al final de la cadena.
+Si se pasa una cadena vacía o puntero nulo devolverá false.
+*/
 bool cad2num(const char *c, double &n);
 
 #endif 
